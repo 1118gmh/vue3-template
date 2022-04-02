28fdcd0928a2a3b1d8b3b4d47847ddfc3c952717
1118gmh <450695071@qq.com>
2022-04-02 16:32:56 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import router from '@/router/index'
+import { createPinia ,PiniaPlugin} from 'pinia';
 
 // import 'element-plus/dist/index.css';
 const app = createApp(App)
+const store = createPinia()
+app.use(store)
 app.use(router)
 app.use(ElementPlus, {
   locale: zhCn
 })
+
 
 app.mount('#app')
